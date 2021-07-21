e95b37770e1c261e7d5fc1831028de2949fc3c3f
snylonue <snylonue@gmail.com>
2021-07-21 09:36:45 +0800

    test: add more tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -5,13 +5,17 @@ mod tests {
     #[test]
     fn cookie() {
         let jar = parse(CONTENT).unwrap();
+        assert_eq!(jar.iter().count(), 6);
         assert_eq!(
             jar.get("first_visit_datetime_pc").map(|c| c.value()),
             Some("2021-07-19+10%3A48%3A50")
         );
+        assert!(jar.get("p_ab_id").map(|c| c.secure()).flatten().unwrap());
         assert_eq!(
-            jar.get("PHPSESSID").map(|c| c.value()),
-            Some("j6amv2igf0cec4fdtld5rre5ud7ig3l2")
+            jar.get("PHPSESSID")
+                .map(|c| c.expires_datetime().unwrap().unix_timestamp()),
+            Some(1626662932)
         );
+        assert_eq!(jar.get("yuid_b").map(|c| c.path()).flatten(), Some("/"))
     }
 }
