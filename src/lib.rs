5ad6df3bb9a0348ce4887097569482b60450b5ae
snylonue <snylonue@gmail.com>
2021-07-22 13:40:18 +0800

    lib: add a `CookieJarBuilder` to wrap apis

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,98 @@ pub use time::OffsetDateTime;
 #[grammar = "cookie.pest"]
 struct CookieParser {}
 
-/// Parses a [`CookieJar`](cookie::CookieJar) from a path
+#[derive(Debug, Default)]
+pub struct CookieJarBuilder {
+    jar: CookieJar,
+}
+
+impl CookieJarBuilder {
+    /// Creates a new `CookieJarBuilder`
+    /// ```
+    /// use nescookie::CookieJarBuilder;
+    ///
+    /// let jar = CookieJarBuilder::new().finish();
+    /// ```
+    pub fn new() -> Self {
+        Self::default()
+    }
+    /// Creates a new `CookieJarBuilder` from a [`CookieJar`](cookie::CookieJar)
+    /// parsed cookies will be added to it
+    pub fn with_jar(jar: CookieJar) -> Self {
+        Self { jar }
+    }
+    /// Opens a file with `path` and parses it as cookies
+    ///
+    /// ```
+    /// use nescookie::CookieJarBuilder;
+    ///
+    /// let jar = CookieJarBuilder::new().open("tests/cookies.txt").unwrap().finish();
+    /// ```
+    pub fn open(self, path: impl AsRef<Path>) -> Result<Self, Error> {
+        self.parse_buffer(BufReader::new(File::open(path)?))
+    }
+    /// Parses cookies from something that implements [`BufRead`](std::io::BufRead)
+    ///
+    /// ```
+    /// use nescookie::CookieJarBuilder;
+    /// use std::io::Cursor;
+    ///
+    /// let buf = Cursor::new(b".pixiv.net	TRUE	/	TRUE	1784339332	p_ab_id	7\n");
+    /// let jar = CookieJarBuilder::new().parse_buffer(buf).unwrap().finish();
+    /// ```
+    pub fn parse_buffer(self, mut buf: impl BufRead) -> Result<Self, Error> {
+        let mut s = String::new();
+        buf.read_to_string(&mut s)?;
+        self.parse(&s)
+    }
+    /// Parses cookies from an str
+    ///
+    /// ```
+    /// use nescookie::CookieJarBuilder;
+    ///
+    /// let content = ".pixiv.net	TRUE	/	TRUE	1784339332	p_ab_id	7\n";
+    /// let jar = CookieJarBuilder::new().parse(content).unwrap().finish();
+    /// ```
+    pub fn parse(mut self, s: &str) -> Result<Self, Error> {
+        let file = CookieParser::parse(Rule::file, s)?.next().unwrap();
+        for c in file
+            .into_inner()
+            .take_while(|r: &Pair<Rule>| !matches!(r.as_rule(), Rule::EOI))
+        {
+            let mut fileds: Pairs<Rule> = c.into_inner();
+            let domain = fileds.next().unwrap().as_str();
+            let path = fileds.next().unwrap().as_str();
+            let secure = fileds.next().unwrap().as_str() == "TRUE"; // this value is either "TRUE" or "FALSE"
+            let expiration: i64 = fileds.next().unwrap().as_str().parse().unwrap();
+            let name = fileds.next().unwrap().as_str();
+            let value = fileds.next().unwrap().as_str();
+            let cookie = Cookie::build(name, value)
+                .domain(domain)
+                .path(path)
+                .secure(secure)
+                .expires(match expiration {
+                    0 => None,
+                    exp => Some(OffsetDateTime::from_unix_timestamp(exp)),
+                })
+                .finish();
+            self.jar.add(cookie.into_owned());
+        }
+        Ok(self)
+    }
+    /// Returns the built `CookieJar`
+    pub fn finish(self) -> CookieJar {
+        self.jar
+    }
+}
+
+/// Opens a file with `path` and parses it as [`CookieJar`](cookie::CookieJar)
 ///
 /// ```
 /// let jar = nescookie::open("tests/cookies.txt").unwrap();
 /// ```
 #[inline]
 pub fn open(path: impl AsRef<Path>) -> Result<CookieJar, Error> {
-    parse_buffer(&mut BufReader::new(File::open(path)?))
+    CookieJarBuilder::new().open(path).map(|jar| jar.finish())
 }
 /// Parses a [`CookieJar`](cookie::CookieJar) from something that implements [`BufRead`](std::io::BufRead)
 ///
@@ -37,10 +121,10 @@ pub fn open(path: impl AsRef<Path>) -> Result<CookieJar, Error> {
 /// let buf = Cursor::new(b".pixiv.net	TRUE	/	TRUE	1784339332	p_ab_id	7\n");
 /// let jar = nescookie::parse_buffer(buf).unwrap();
 /// ```
-pub fn parse_buffer(mut buf: impl BufRead) -> Result<CookieJar, Error> {
-    let mut s = String::new();
-    buf.read_to_string(&mut s)?;
-    parse(&s)
+pub fn parse_buffer(buf: impl BufRead) -> Result<CookieJar, Error> {
+    CookieJarBuilder::new()
+        .parse_buffer(buf)
+        .map(|jar| jar.finish())
 }
 /// Parses a [`CookieJar`](cookie::CookieJar) from an str
 ///
@@ -49,29 +133,5 @@ pub fn parse_buffer(mut buf: impl BufRead) -> Result<CookieJar, Error> {
 /// let jar = nescookie::parse(content).unwrap();
 /// ```
 pub fn parse(s: &str) -> Result<CookieJar, Error> {
-    let file = CookieParser::parse(Rule::file, s)?.next().unwrap();
-    let mut jar = CookieJar::new();
-    for c in file
-        .into_inner()
-        .take_while(|r: &Pair<Rule>| !matches!(r.as_rule(), Rule::EOI))
-    {
-        let mut fileds: Pairs<Rule> = c.into_inner();
-        let domain = fileds.next().unwrap().as_str();
-        let path = fileds.next().unwrap().as_str();
-        let secure = fileds.next().unwrap().as_str() == "TRUE"; // this value is either "TRUE" or "FALSE"
-        let expiration: i64 = fileds.next().unwrap().as_str().parse().unwrap();
-        let name = fileds.next().unwrap().as_str();
-        let value = fileds.next().unwrap().as_str();
-        let cookie = Cookie::build(name, value)
-            .domain(domain)
-            .path(path)
-            .secure(secure)
-            .expires(match expiration {
-                0 => None,
-                exp => Some(OffsetDateTime::from_unix_timestamp(exp)),
-            })
-            .finish();
-        jar.add(cookie.into_owned());
-    }
-    Ok(jar)
+    CookieJarBuilder::new().parse(s).map(|jar| jar.finish())
 }
